adc83a8c19df8c8b9a8d219596eda843e47baa78
Alexander <mmaulwurff@gmail.com>
2018-08-07 08:01:00 +0700

    remove Laser Sight files

--- build.sh
+++ build.sh
@@ -31,10 +31,7 @@ zip weapon-menu.pk3 \
     *.md \
     *.txt \
     language.enu \
-    textures.blue \
-    textures.green \
     textures.icons \
-    textures.txt \
 && \
 cp weapon-menu.pk3 weapon-menu-$(git describe --abbrev=0 --tags).pk3 \
 && \
