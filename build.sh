fc66d6bc46f2aca4b4f81f5d815ea7c4db267ab7
Alexander Kromm <mmaulwurff@gmail.com>
2020-01-18 10:54:27 +0700

    clean up the build script

--- build.sh
+++ build.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-#IWAD=~/Programs/Games/wads/doom/freedoom1.wad
-#IWAD=~/Programs/Games/wads/doom/HERETIC.WAD
-#IWAD=~/Programs/Games/wads/modules/game/harm1.wad
+set -e
 
-acc source/weapon-menu.acs acs/weapon-menu.o \
-&& \
-rm -f weapon-menu.pk3 \
-&& \
+name=weapon-menu-$(git describe --abbrev=0 --tags).pk3
+
+acc source/weapon-menu.acs acs/weapon-menu.o
+
+rm -f "$name"
+
 git log --date=short --pretty=format:"-%d %ad %s%n" | \
     grep -v "^$" | \
     sed "s/HEAD -> master, //" | \
@@ -15,30 +15,18 @@ git log --date=short --pretty=format:"-%d %ad %s%n" | \
     sed "s/ (HEAD -> master)//" | \
     sed "s/ (origin\/master)//"  |\
     sed "s/- (tag: \(v\?[0-9.]*\))/\n\1\n-/" \
-    > changelog.txt \
-&& \
-zip weapon-menu.pk3 \
-    acs/weapon-menu.o \
-    filter/*/language.enu \
-    sounds/*.wav  \
-    sounds/*.lmp  \
-    source/*.acs  \
-    sprites/*.png \
-    sprites/*.lmp \
-    zscript/*.zs  \
-    *.lmp  \
-    *.fon2 \
-    *.md   \
-    *.txt  \
-    *.zs   \
-    language.enu \
-&& \
-cp weapon-menu.pk3 weapon-menu-$(git describe --abbrev=0 --tags).pk3 \
-&& \
-gzdoom -iwad $IWAD \
-       -file \
-       weapon-menu.pk3 \
-       ~/Programs/Games/wads/maps/DOOMTEST.wad \
-       "$1" "$2" \
-       +map test \
-       -nomonsters
+    > changelog.txt
+
+zip -R "$name" \
+    "*.acs"  \
+    "*.enu"  \
+    "*.fon2" \
+    "*.lmp"  \
+    "*.md"   \
+    "*.o"    \
+    "*.png"  \
+    "*.txt"  \
+    "*.wav"  \
+    "*.zs"
+
+gzdoom -file "$name" "$@"
